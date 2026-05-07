692bc7a4ee761c0cc8a58045f4570e9268958ee3
Andrea Escolar <andrea.escolar.artigas@ibadia.cat>
2026-05-07 21:48:47 +0200

    chore(config): configurar PrimeVue 4 Aura, Router y servicios

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,25 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // import router from './router'
 
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import 'primeicons/primeicons.css'
+import ConfirmationService from 'primevue/confirmationservice'
+import ToastService from 'primevue/toastservice'
+import { Theme } from '@primevue/themes'
+
 const app = createApp(App)
 
 // app.use(router)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.dark-mode'
+        }
+    }
+})
+app.use(ConfirmationService)
+app.use(ToastService)
 
 app.mount('#app')
