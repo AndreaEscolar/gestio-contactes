d4669679d0e94664bb4fd42fb813cfb85ac0fc0d
Andrea Escolar <andrea.escolar.artigas@ibadia.cat>
2026-05-17 09:32:22 +0200

    refactor(router): añadir meta titles, afterEach

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
+import ContactsView from "../views/ContactsView.vue";
+import GroupsView from "../views/GroupsView.vue";
+import HistoryView from "../views/HistoryView.vue";
+import StatsView from "../views/StatsView.vue";
 
-const route = createRouter({
+
+const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
 
     routes: [
@@ -12,21 +17,25 @@ const route = createRouter({
             path: '/contacts',
             name: 'contacts',
             component: () => import('../views/ContactsView.vue'),
+            meta: { title: 'Contactos'}
         },
         {
             path: '/groups',
             name: 'groups',
             component: () => import('../views/GroupsView.vue'),
+            meta: { title: 'Grupos'}
         },
         {
             path: '/history',
             name: 'history',
             component: () => import('../views/HistoryView.vue'),
+            meta: { title: 'Historial'}
         },
         {
             path: '/stats',
             name: 'stats',
             component: () => import('../views/StatsView.vue'),
+            meta: { title: 'Estadisticas'}
         },
         {
             path: `/:pathMatch(.*)*`,
@@ -35,4 +44,8 @@ const route = createRouter({
     ]
 })
 
-export default route
+router.afterEach(to => {
+    document.title = `${to.meta.title ?? 'App'} - Gestión de Contactos`
+})
+
+export default router
